4ebf497fd5248f715213fa3b3cdd737f2c317b10
Bruno Godinez <bruno.gomd@gmail.com>
2024-04-12 19:15:00 -0600

    Guardado_3

--- Doctor.java
+++ Doctor.java
@@ -2,6 +2,10 @@ class Doctor {
    String especialidad, nombre;
    int edad;
 
+   Doctor() {
+      System.out.println("Se esta construyendo el metodo constructor");
+   }
+
    public void mostrarEspecialidad() {
       System.out.println("Hola mi especialista es: " + this.especialidad);
    }
--- MainDoctor.java
+++ MainDoctor.java
@@ -1,12 +1,22 @@
 class MainDoctor{
-   public static void main(String[]args){
+   public static void main(String[] args) {
       Doctor Oscaar = new Doctor();
-      Oscaar.especialidad ="Cardiologia";
-      Oscaar.edad  =26;
+      Oscaar.especialidad = "Cardiologia";
+      Oscaar.edad = 26;
       Oscaar.nombre = "Oscaar";
-      
+
       Oscaar.mostrarEspecialidad();
       Oscaar.mostrarEdad();
-      Oscaar.mostrarNombre(); 
+      Oscaar.mostrarNombre();
+   
+      Doctor Brunoo = new Doctor();
+      
+      Brunoo.especialidad ="Poodologo";
+      Brunoo.edad  =24;
+      Brunoo.nombre = "Brunoo";
+      
+      Brunoo.mostrarEspecialidad();
+      Brunoo.mostrarEdad();
+      Brunoo.mostrarNombre(); 
    }
 }
